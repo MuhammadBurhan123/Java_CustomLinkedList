f9ee22e23e0eca72d9658560a5b16b1aa2a41636
MuhammadBurhan123 <burhantheschoolboy@gmail.com>
2024-10-14 08:05:59 +0500

    Updated Main

--- Main.java
+++ Main.java
@@ -4,10 +4,26 @@ class Main{
 
          LinkList list = new LinkList();
 
-        list.add(5);
-        list.add(3);
-        list.add(6);
-
-        list.display();
+         System.out.println("Added 4,5,6 to end of list");
+         list.addLast(4);
+         list.addLast(5);
+         list.addLast(6);
+         list.display();
+         
+         System.out.println("Added 0,1,2,3 to start of list");
+         list.addFirst(3);
+         list.addFirst(2);
+         list.addFirst(1);
+         list.addFirst(0);
+         list.display();
+         
+         System.out.println("Delete first item");
+         list.deleteFirst();
+         list.display();
+         
+         System.out.println("Delete first item");
+         list.deleteLast();
+         list.display();
+        
     }
 }
